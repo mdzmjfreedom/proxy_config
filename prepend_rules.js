e208d2618ecbb4f511db6c33863cd8ed7d6bb0e3
Freedom <mengdi@ztocc.com>
2025-11-19 11:04:34 +0800

    修改

---
+++ prepend_rules.js
@@ -0,0 +1,26 @@
+// Stash: HTTP Response Script
+// 功能：将自定义规则插入到 rules 最前面（不排序、不修改原有规则）
+
+// 在这里填你的规则（按顺序）
+const customRules = [
+    "DOMAIN-SUFFIX,smoot.apple.com,🇺🇲 美国Z01",
+    "DOMAIN,guzzoni.apple.com,🇺🇲 美国Z01",
+    "DOMAIN,apple-relay.cloudflare.com,🇺🇲 美国Z01",
+    "DOMAIN,apple-relay.fastly-edge.com,🇺🇲 美国Z01",
+    "DOMAIN,cp4.cloudflare.com,🇺🇲 美国Z01",
+    "DOMAIN,apple-relay.apple.com,🇺🇲 美国Z01"
+];
+
+module.exports = async function (resp) {
+    let config = JSON.parse(resp.body);
+
+    // 原 config 中可能没有 rules
+    let rules = config.rules || [];
+
+    // 将自定义规则追加到最前面
+    config.rules = [...customRules, ...rules];
+
+    return {
+        body: JSON.stringify(config)
+    };
+};
